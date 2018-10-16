73881a02847d6ae1ede08c7ffd95a37d351e454e
Gary Talent <gtalent2@gmail.com>
2018-10-15 22:20:44 -0500

    Add network transfer of log messages

--- gdblogger.py
+++ gdblogger.py
@@ -1,34 +1,54 @@
 
 import json
+import os
+import socket
+
+log_server = None
+
+class Msg(dict):
+    def __init__(self, type, data):
+        dict.__init__(self, type=type, data=data)
+
+class InitMsg(dict):
+    def __init__(self, cmd):
+        dict.__init__(self, cmd=cmd)
 
 class Field(dict):
     name = ''
     value = None
     type = ''
+    fields = None
     def __init__(self, name, value, field_type):
         self.name = name
-        self.value = value
         if field_type == gdb.TYPE_CODE_STRUCT:
             self.type = 'struct'
+            self.fields = value
         elif field_type == gdb.TYPE_CODE_UNION:
             self.type = 'union'
+            self.value = value
         elif field_type == gdb.TYPE_CODE_ENUM:
             self.type = 'enum'
+            self.value = value
         elif field_type == gdb.TYPE_CODE_ARRAY:
-            self.type = 'enum'
+            self.type = 'array'
+            self.fields = value
         else:
             self.type = 'scalar'
-        dict.__init__(self, name=name, value=value, type=self.type)
+            self.value = value
+        dict.__init__(self, name=name, value=self.value, fields=self.fields, type=self.type)
 
 class Frame(dict):
-    trace_msg = ''
     function = ''
-    file_name = ''
-    line_number = 0
+    file = ''
+    line = 0
     fields = []
-    def __init__(self, trace_msg, function, file_name, line_number, fields):
-        dict.__init__(self, trace_msg=trace_msg, function=function, \
-                      file_name=file_name, line_number=line_number, fields=fields)
+    def __init__(self, function, file, line, fields):
+        dict.__init__(self, function=function, file=file, \
+                      line=line, fields=fields)
+
+class TraceEvent(dict):
+    def __init__(self, channel, log_msg, frames):
+        dict.__init__(self, channel=channel, log_msg=log_msg, frames=frames)
 
 class Executor:
     def __init__(self, cmd):
@@ -70,13 +90,15 @@ def frame_source_line(bt, level):
     return (file, line)
 
 def handle_tracepoint(event):
+    global log_server
     gdb.execute("set scheduler-locking on")
     # iterate over frames
     gdb.newest_frame()
     cur_frame = gdb.selected_frame()
-    print('Frames')
+    #print('Frames')
     frame_no = 0
     frames = []
+    log_msg = ''
     bt = gdb.execute('bt', to_string=True).split('\n')
     bt.pop()
     while cur_frame is not None:
@@ -87,10 +109,10 @@ def handle_tracepoint(event):
         line_number = source_line[1]
         fields = []
         frame_symbols = set()
-        print('{}: {}'.format(frame_no, cur_frame.name()))
-        print('\tArchitecture: ' + cur_frame.architecture().name())
+        #print('{}: {}'.format(frame_no, cur_frame.name()))
+        #print('\tArchitecture: ' + cur_frame.architecture().name())
         # iterate over blocks
-        print('\tVariables:')
+        #print('\tVariables:')
         block = cur_frame.block()
         log_msg = ''
         while block is not None:
@@ -102,8 +124,10 @@ def handle_tracepoint(event):
                     #print(help(symbol.symtab))
                     sym_val = symbol.value(cur_frame)
                     field_value = build_value(sym_val)
+                    if field_name == 'ox::trace::debugger::channel':
+                        channel = sym_val.string()
                     if field_name == 'ox::trace::debugger::logMsg':
-                        log_msg = field_value
+                        log_msg = sym_val.string()
                     else:
                         fields.append(Field(field_name, field_value, sym_val.type.code))
                         #print('\t\t{}: {}'.format(field_name, json.dumps(field_value)))
@@ -112,16 +136,34 @@ def handle_tracepoint(event):
             block = block.superblock
         # end: iterate over blocks
         cur_frame = cur_frame.older()
-        frames.append(Frame(log_msg, function, file_name, line_number, fields))
+        frames.append(Frame(function, file_name, line_number, fields))
         frame_no += 1
     # end: iterate over frames
-    gdb.execute("set scheduler-locking off")
+    gdb.execute('set scheduler-locking off')
     gdb.post_event(Executor("continue"))
-    print(json.dumps(frames, indent=4, separators=(',', ': ')))
-    print("Finished safely")
+    msg = json.dumps(Msg('TraceEvent', TraceEvent(channel, log_msg, frames)))
+    if log_server is not None:
+        log_server.send(bytes(msg, 'utf-8'))
+    print('Finished safely')
 
 def handle_exit(event):
-    gdb.post_event(Executor("quit"))
+    if log_server != None:
+        log_server.close()
+        gdb.post_event(Executor('quit'))
+
+def connect_to_server(ip_port):
+    ip_port = ip_port.split(':')
+    ip_port = (ip_port[0], int(ip_port[1]))
+    global log_server
+    log_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    log_server.connect(ip_port)
+
+ip_port = os.environ.get('LOG_SERVER')
+if ip_port is not None:
+    connect_to_server(ip_port)
 
 gdb.events.stop.connect(handle_tracepoint)
 gdb.events.exited.connect(handle_exit)
+
+if log_server is not None:
+    log_server.send(bytes(json.dumps(Msg('Init', InitMsg(''))), 'utf-8'))
